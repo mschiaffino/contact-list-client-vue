5fc03368368022cd0944cc61e2328de22d7cd967
Martín Schiaffino <schiaffinomartin@gmail.com>
2017-07-22 10:38:50 -0300

    Improved contact details component and added contact action buttons component

---
+++ src/components/ContactActionButtons.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-fab-transition>
+    <v-speed-dial v-model="fab" bottom right direction="top" hover transition="slide-y-transition" v-show="show">
+      <v-btn slot="activator" class="blue darken-2" dark fab hover>
+        <v-icon>account_circle</v-icon>
+        <v-icon>close</v-icon>
+      </v-btn>
+      <v-btn fab dark small class="green">
+        <v-icon>edit</v-icon>
+      </v-btn>
+      <v-btn fab dark small class="red">
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-speed-dial>
+  </v-fab-transition>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      show: false
+    }
+  },
+  mounted() {
+    this.show = true
+  }
+}
+</script>
+
+
+<style>
+.speed-dial {
+  position: absolute;
+}
+
+.btn--floating {
+  position: relative;
+}
+</style>
--- src/components/ContactDetails.vue
+++ src/components/ContactDetails.vue
@@ -1,57 +1,45 @@
 <template>
-  <v-layout column class="contact-details" mx-3>
+  <v-layout column class="mx-3">
     <span v-text="contactFullName(contactById(id))" class="my-5 display-2 text-xs-center"></span>
   
-    <v-layout column mt-5>
-      <v-layout my-4 row>
-        <v-flex xs4>
-          <label class="title">First name</label>
-        </v-flex>
-        <v-flex xs4>
-          <span v-text="contact.firstName"></span>
-        </v-flex>
-      </v-layout>
+    <v-flex xs8 offset-xs2>
+      <v-text-field :disabled="!editing" v-model="contact.firstName" name="firstName" label="First name"></v-text-field>
+    </v-flex>
   
-      <v-layout my-4 row>
-        <v-flex xs4>
-          <label class="title">Last name</label>
-        </v-flex>
-        <v-flex xs4>
-          <span v-text="contact.lastName"></span>
-        </v-flex>
-      </v-layout>
+    <v-flex xs8 offset-xs2>
+      <v-text-field :disabled="!editing" v-model="contact.lastName" name="lastName" label="Last name"></v-text-field>
+    </v-flex>
   
-      <v-layout my-4 row>
-        <v-flex xs4>
-          <label class="title">Date of birth</label>
-        </v-flex>
-        <v-flex xs4>
-          <span v-text="contact.dateOfBirth"></span>
-        </v-flex>
-      </v-layout>
+    <v-flex xs8 offset-xs2>
+      <v-menu :disabled="!editing" lazy :close-on-content-click="false" v-model="menu" transition="scale-transition" offset-y full-width :nudge-left="40" max-width="290px">
+        <v-text-field slot="activator" label="Date of birth" :disabled="!editing" v-model="contact.dateOfBirth" append-icon="event" readonly></v-text-field>
+        <v-date-picker :disabled="!editing" v-model="contact.dateOfBirth" scrollable>
+          <template scope="{ save, cancel }">
+            <v-card-actions>
+              <v-btn flat primary @click.native="cancel()">Cancel</v-btn>
+              <v-btn flat primary @click.native="save()">Save</v-btn>
+            </v-card-actions>
+          </template>
+        </v-date-picker>
+      </v-menu>
+    </v-flex>
   
-      <v-layout my-4 row>
-        <v-flex xs4>
-          <label class="title">Phone number</label>
-        </v-flex>
-        <v-flex xs4>
-          <span v-text="contact.phone"></span>
-        </v-flex>
-      </v-layout>
+    <v-flex xs8 offset-xs2>
+      <v-text-field :disabled="!editing" v-model="contact.phone" name="phone" label="Phone number"></v-text-field>
+    </v-flex>
   
-      <v-layout my-4 row>
-        <v-flex xs4>
-          <label class="title">Zip code</label>
-        </v-flex>
-        <v-flex xs4>
-          <span v-text="contact.zipCode"></span>
-        </v-flex>
-      </v-layout>
+    <v-flex xs8 offset-xs2>
+      <v-text-field :disabled="!editing" v-model="contact.zipCode" name="zipCode" label="Zip code"></v-text-field>
+    </v-flex>
+  
+    <v-layout justify-end>
+      <contact-action-buttons></contact-action-buttons>
     </v-layout>
   </v-layout>
 </template>
 
 <script>
+import ContactActionButtons from './ContactActionButtons'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -62,13 +50,11 @@ export default {
 
     contact() {
       return this.contactById(this.id)
-    }
+    },
+    editing() { return false }
+  },
+  components: {
+    'contact-action-buttons': ContactActionButtons
   }
 }
 </script>
-
-<style lang="stylus">
-.contact-details{
-  
-}
-</style>
