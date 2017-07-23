955c277d2229fb5412c3b322c9a593a75c03b4bc
Martín Schiaffino <schiaffinomartin@gmail.com>
2017-07-23 02:41:23 -0300

    Styles changes to make NoContactSelected component a little bit more responsive

--- src/components/NoContactSelected.vue
+++ src/components/NoContactSelected.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout column justify-space-between class="full-height">
     <v-flex class="text-xs-center content">
-      <img src="../assets/contact.png">
+      <img src="../assets/contact.png" class="contact-image">
       <h2 class="blue-grey--text text--lighten-2">No contact selected</h2 class="blue-grey lighten-3">
     </v-flex>
   </v-layout>
@@ -13,6 +13,11 @@
       flex-direction: column;
       align-items: center;
       justify-content: center;
+
+      .contact-image{
+        max-width: 50%;
+        max-height: 50%;
+      }
   }
 </style>
 
